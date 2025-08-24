013a0d35a415e67404d8771b5cc1d3f728dbb2dd
SachinBhawala <sachinkbse789@gmail.com>
2025-08-24 21:53:42 +0530

    Day 11 Final Commit

---
+++ 6_Basic_Hashing/2_Character_Hashing/1_HashArray.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+
+    // Create a hash array
+    int hash[26] = {0};
+
+    // Hashing the characters
+    for (char c : s) {
+        hash[c - 'a']++;
+    }
+
+    int q;
+    cout << "Enter number of queries: ";
+    cin >> q;
+    while (q--) {
+        char c;
+        cout << "Enter a character to query frequency: ";
+        cin >> c;
+        if (c >= 'a' && c <= 'z') {
+            cout << "Frequency of '" << c << "': " << hash[c - 'a'] << endl;
+        } else {
+            cout << "Invalid character. Please enter a lowercase letter." << endl;
+        }
+    }
+    return 0;
+}
