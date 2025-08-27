df45f4f5ca7445a46e3826170a54c950ef23aaf5
SachinBhawala <sachinkbse789@gmail.com>
2025-08-27 22:42:36 +0530

    Day 13 Final Commit

--- 7_Imp_Sorting_Techniques/2_BubbleSort.cpp
+++ 7_Imp_Sorting_Techniques/2_BubbleSort.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
-    
+    vector<int> arr = {10, -80, 60, 30, -70, 90, 40, -50};
+    int n = arr.size();
+    bool alreadySorted = true;
+    for(int i = 0; i < n; i++) {
+        int didswap = 0;
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                didswap = 1;
+                alreadySorted = false;
+            }
+        }
+        if(didswap == 0) break;
+    }
+    if (alreadySorted) {
+        cout << "Already sorted" << endl;
+    }
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
--- 7_Imp_Sorting_Techniques/3_InsertionSort.cpp
+++ 7_Imp_Sorting_Techniques/3_InsertionSort.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    vector<int> arr = {90, 80, 70, 60, 50, 40, 30, 20, 10};
     
     return 0;
 }
