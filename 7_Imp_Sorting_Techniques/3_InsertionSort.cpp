8652546222c8c6a05477d828e2cab5eeed7dd2f8
SachinBhawala <sachinkbse789@gmail.com>
2025-10-27 20:48:31 +0530

    Day 14 Final Commit

--- 7_Imp_Sorting_Techniques/3_InsertionSort.cpp
+++ 7_Imp_Sorting_Techniques/3_InsertionSort.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
     vector<int> arr = {90, 80, 70, 60, 50, 40, 30, 20, 10};
-    
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        int j = i;
+        while(j > 0 && arr[j - 1] > arr[j]) {
+            swap(arr[j-1], arr[j]);
+            j--;
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
