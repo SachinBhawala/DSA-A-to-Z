fdf4a293467f2292b1cf2ad82cb1be2ab4719898
SachinBhawala <sachinkbse789@gmail.com>
2025-11-06 21:37:04 +0530

    Day 23 Final Commit

--- 8_Array_Problems/1_Easy_Problems/13_FindTheNumberThatAppearsOnceAndOtherNumbersTwice.cpp
+++ 8_Array_Problems/1_Easy_Problems/13_FindTheNumberThatAppearsOnceAndOtherNumbersTwice.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
+    vector<int> arr = {1, 1, 2, 3, 3, 4, 4};
+
+    // Brute Forch Approach
+    int count;
+    for(int i = 0; i < arr.size(); i++) {
+        count = 0;
+        for(int j = 0; j < arr.size(); j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if(count == 1) {
+            cout << arr[i] << " appears one time...!!!";
+            break;
+        }
+    }
+
+    cout << endl;
+
+    // Better Approach
     
 
     return 0;
