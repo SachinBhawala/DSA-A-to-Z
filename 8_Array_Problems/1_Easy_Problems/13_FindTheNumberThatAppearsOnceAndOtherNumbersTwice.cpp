7b46d7fb3856cf12d40dc22b995c7b393d9b1627
SachinBhawala <sachinkbse789@gmail.com>
2025-11-08 21:18:07 +0530

    Day 24 Final Commit

--- 8_Array_Problems/1_Easy_Problems/13_FindTheNumberThatAppearsOnceAndOtherNumbersTwice.cpp
+++ 8_Array_Problems/1_Easy_Problems/13_FindTheNumberThatAppearsOnceAndOtherNumbersTwice.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    vector<int> arr = {1, 1, 2, 3, 3, 4, 4};
+    vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
 
-    // Brute Forch Approach
+    // Brute Forch Approach : TC = O(n^2), SC = O(1)
     int count;
     for(int i = 0; i < arr.size(); i++) {
         count = 0;
@@ -21,8 +21,43 @@ int main() {
 
     cout << endl;
 
-    // Better Approach
-    
+    // Better Approach : TC = O(n), SC = O(max element)
+    int maxi = 0;
+    for(int i : arr) {
+        maxi = max(i, maxi);
+    }
+    vector<int> hash(maxi + 1, 0);
+    for(int i = 0; i < arr.size(); i++) {
+        hash[arr[i]]++;
+    }
+    for(int i = 1; i < hash.size(); i++) {
+        if(hash[i] == 1) {
+            cout << i << " appears one time...!!!";
+            break;
+        }
+    }
+
+    cout << endl;
+
+    // We can also use map data structure here : TC = O(n log n), SC = O(n/2 + 1)
+    map<int, int> m;
+    for(int i : arr) {
+        m[i]++;
+    }
+    for(int i : arr) {
+        if(m[i] == 1) {
+            cout << i << " appears one time...!!!";
+        }
+    }
+
+    cout << endl;
+
+    // Optimal Approach : TC = O(n), SC = O(1)
+    int x = 0;
+    for(int i : arr) {
+        x = x ^ i;
+    }
+    cout << x << " appears one time...!!!";
 
     return 0;
 }
