007bf497a7a66d5fc5ffc84519c65721361b39a0
SachinBhawala <sachinkbse789@gmail.com>
2025-11-14 21:19:42 +0530

    Day 28 Final Commit

---
+++ 8_Array_Problems/2_Medium_Problems/1_2Sum.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    // Brute Force Approach : TC = O(n^2), SC = O(1)
+    for(int i = 0; i < arr.size(); i++) {
+        for(int j = i + 1; j < arr.size(); j++) {
+            if(arr[i] + arr[j] == target) {
+                cout << "Yes\n" << "Index: " << i << "," << j;
+                break;
+            }
+        }
+    }
+
+    cout << endl;
+
+    // Better Approach : TC = O(n logn), SC = O(n)
+    map<int, int> m;
+    int flag = false;
+    for(int i = 0; i < arr.size(); i++) {
+        int more = target - arr[i];
+        if(m.find(more) != m.end()) {
+            cout << "Yes\n" << "Index: " << m[more] << "," << i;
+            flag = true;
+            break;
+        }
+        m[arr[i]] = i;
+    }
+    if(flag == false) {
+        cout << "No";
+    }
+
+    cout << endl;
+
+    // Optimal Approach
+    
+
+    return 0;
+}
