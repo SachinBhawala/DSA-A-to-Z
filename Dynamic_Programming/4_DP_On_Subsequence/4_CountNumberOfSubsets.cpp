a61f95a11143becce8eeb45a1a67bb3b46416869
SachinBhawala <sachinkbse789@gmail.com>
2026-01-18 22:45:06 +0530

    Day 43 Final Commit

--- Dynamic_Programming/4_DP_On_Subsequence/4_CountNumberOfSubsets.cpp
+++ Dynamic_Programming/4_DP_On_Subsequence/4_CountNumberOfSubsets.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 // Recursion : TC = O(2^n), SC = O(n)
 int countSubset(int index, int target, vector<int>& arr) {
-    // if(index == 0) {
-    //     if(target == 0 && arr[0] == 0) return 2;
-    //     if(target == 0 || arr[0] == target) return 1;
-    //     return 0;
-    // }
+    if(index == 0) {
+        if(target == 0 && arr[0] == 0) return 2;
+        if(target == 0 || arr[0] == target) return 1;
+        return 0;
+    }
 
-    if(target == 0) return 1;
-    if(index == 0) return (arr[0] == target);
+    // if(target == 0) return 1;
+    // if(index == 0) return (arr[0] == target);
 
     int notTake = countSubset(index-1, target, arr);
     int take = 0;
@@ -21,14 +21,14 @@ int countSubset(int index, int target, vector<int>& arr) {
 
 // DP (Mamoization) : TC = O(n*k), SC = O(n*k + n)
 int countSubsetDP1(int index, int target, vector<int>& arr, vector<vector<int>>& dp) {
-    // if(index == 0) {
-    //     if(target == 0 && arr[0] == 0) return 2;
-    //     if(target == 0 || arr[0] == target) return 1;
-    //     return 0;
-    // }
+    if(index == 0) {
+        if(target == 0 && arr[0] == 0) return 2;
+        if(target == 0 || arr[0] == target) return 1;
+        return 0;
+    }
 
-    if(target == 0) return 1;
-    if(index == 0) return (arr[0] == target);
+    // if(target == 0) return 1;
+    // if(index == 0) return (arr[0] == target);
 
     if(dp[index][target] != -1) return dp[index][target];
 
@@ -43,24 +43,24 @@ int countSubsetDP1(int index, int target, vector<int>& arr, vector<vector<int>>&
 int countSubsetDP2(int n, int k, vector<int>& arr) {
     vector<vector<int>> dp(n, vector<int> (k+1, 0));
 
-    // if (arr[0] == 0) {
-    //     dp[0][0] = 2;          // pick 0 OR not pick 0
-    // } else {
-    //     dp[0][0] = 1;          // empty subset
-    // }
+    if (arr[0] == 0) {
+        dp[0][0] = 2;          // pick 0 OR not pick 0
+    } else {
+        dp[0][0] = 1;          // empty subset
+    }
 
-    // if (arr[0] != 0 && arr[0] <= k) {
-    //     dp[0][arr[0]] = 1;     // single-element subset
-    // }
+    if (arr[0] != 0 && arr[0] <= k) {
+        dp[0][arr[0]] = 1;     // single-element subset
+    }
 
     for(int index = 0; index < n; index++) {
         dp[index][0] = 1;
     }
-    if(arr[0] <= k) dp[0][arr[0]] = 1;
+    // if(arr[0] <= k) dp[0][arr[0]] = 1;
 
     for(int index = 1; index < n; index++) {
-        for(int target = 1; target <= k; target++) {
-        // for(int target = 0; target <= k; target++) {
+        // for(int target = 1; target <= k; target++) {
+        for(int target = 0; target <= k; target++) {
             int notTake = dp[index-1][target];
             int take = 0;
             if(arr[index] <= target) take = dp[index-1][target-arr[index]];
@@ -75,23 +75,22 @@ int countSubsetDP2(int n, int k, vector<int>& arr) {
 int countSubsetDP3(int n, int k, vector<int>& arr) {
     vector<int> prev(k+1, 0);
 
-    // if (arr[0] == 0) {
-    //     prev[0] = 2;      // pick 0 OR not pick 0
-    // } else {
-    //     prev[0] = 1;      // empty subset
-    // }
+    if (arr[0] == 0) {
+        prev[0] = 2;      // pick 0 OR not pick 0
+    } else {
+        prev[0] = 1;      // empty subset
+    }
 
-    // if (arr[0] != 0 && arr[0] <= k) {
-    //     prev[arr[0]] = 1;
-    // }
+    if (arr[0] != 0 && arr[0] <= k) {
+        prev[arr[0]] = 1;
+    }
 
-    prev[0] = 1;
-    if(arr[0] <= k) prev[arr[0]] = 1;
+    // prev[0] = 1;
+    // if(arr[0] <= k) prev[arr[0]] = 1;
 
     for(int index = 1; index < n; index++) {
         vector<int> temp(k+1, 0);
-        temp[0] = 1;
-        for(int target = 1; target <= k; target++) {
+        for(int target = 0; target <= k; target++) {
             int notTake = prev[target];
             int take = 0;
             if(arr[index] <= target) take = prev[target-arr[index]];
